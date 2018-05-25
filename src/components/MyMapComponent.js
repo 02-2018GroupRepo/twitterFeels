f621271b4825a065d57d66d00370d40ba20e8c31
Shahar Levy <shaharlevy71@gmail.com>
2018-05-25 14:03:09 -0400

    Adds image folder and removed overlaying text from icon images

--- src/components/MyMapComponent.js
+++ src/components/MyMapComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-
 const testIcon = require('../face.png');
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
@@ -10,7 +9,7 @@ const MyMapComponent = withScriptjs(withGoogleMap((props) =>
         defaultZoom={7}
         defaultCenter={{ lat: 27.1750, lng: 78.0422 }}
     >
-        {props.isMarkerShown && <Marker defaultIcon={testIcon} label={"Taj Mahal"} position={{ lat: 27.1750, lng: 78.0422 }} />}
+        {props.isMarkerShown && <Marker defaultIcon={testIcon} position={{ lat: 27.1750, lng: 78.0422 }} />}
     </GoogleMap>
 ));
 
