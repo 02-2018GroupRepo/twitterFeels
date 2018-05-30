a6e7528d5d83c0ba62e504c69a31f7ffa5f28754
Shahar Levy <shaharlevy71@gmail.com>
2018-05-30 09:19:13 -0400

    Changed images to circles, moved image file

--- src/components/MyMapComponent.js
+++ src/components/MyMapComponent.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 const testIcon = require('../face.png');
+const faceOne = require('../Images/CircularFace1.png');
+const faceTwo = require('../Images/CircularFace2.png');
+const faceThree = require('../Images/CircularFace3.png');
+const faceFour = require('../Images/CircularFace4.png');
+const faceFive = require('../Images/CircularFace5.png');
+
+
 const markers = [{ lat: 27.1750, lng: 78.0422, id: 1 },{ lat: 57.1750, lng: 98.0422, id: 2 },{ lat: 17.1750, lng: 78.0422, id: 3 },{ lat: 17.1750, lng: 48.0422, id: 4 },{ lat: 27.1750, lng: 8.0422, id: 5 },{ lat: 57.1750, lng: 78.0422, id: 6 }];
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
